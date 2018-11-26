dc8055a2c86edd39e9e3ad0cc9a76b04ed7d3a8d
Letícia <leticialeitecaetano17@gmail.com>
2018-11-26 00:09:42 -0200

    Formulario de fale conosco

--- djangoscourse/settings.py
+++ djangoscourse/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'widget_tweaks',
     'core',
     'catalog',
 ]
@@ -124,3 +125,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# E-mail
+EMAIL_HOST = ''
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+DEFAULT_FROM_EMAIL = 'leticialeitecaetano@hotmail.com'
--- djangoscourse/urls.py
+++ djangoscourse/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from core import views
-
+from catalog import views as views_catalog
 
 urlpatterns = [
     url(r'^$', views.index,name='index'),
